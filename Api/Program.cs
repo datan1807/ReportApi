03cafa7665d97c723b14eedbd491f64664a92d49
datndse62825@fpt.edu.vn <datndse62825@fpt.edu.vn>
2022-01-21 13:29:27 +0700

    config

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,7 @@ using Api.Services;
 using Api.Services.IService;
 using Api.UnitOfWorks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,29 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "ToDo API",
+            Description = "Report Management ASP.NET Core Web API",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Nguyen Duy Dat",
+                Email = string.Empty,
+                Url = new Uri("https://twitter.com/spboyer"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Use under LICX",
+                Url = new Uri("https://example.com/license"),
+            }
+        });
+    });
+
 
 builder.Services.AddDbContext<Api.Data.QlreportContext>(
     options =>
@@ -26,15 +49,19 @@ builder.Services.AddDbContext<Api.Data.QlreportContext>(
         options.UseSqlServer(builder.Configuration.GetConnectionString("QLReportDB"));
     }
     );
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+
     app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+    app.UseSwaggerUI(
+        c =>
+        {
+            c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Report Management System V1");
+        });
+
 
 app.UseHttpsRedirection();
 
